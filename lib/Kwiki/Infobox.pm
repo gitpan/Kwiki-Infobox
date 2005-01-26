a8185b60eee98f558803f3d46af1264d2e8e680e
劉康民 <gugod@gugod.org>
2005-01-26 10:02:49 +0000

    Import of GUGOD/Kwiki-Infobox-0.02 from CPAN.
    gitpan-cpan-distribution: Kwiki-Infobox
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         GUGOD/Kwiki-Infobox-0.02.tar.gz
    gitpan-cpan-author:       GUGOD
    gitpan-cpan-maturity:     released

--- lib/Kwiki/Infobox.pm
+++ lib/Kwiki/Infobox.pm
@@ -34,7 +34,7 @@ idea about how to do this magically. Please discuss with me.
 use strict;
 use warnings;
 use Kwiki::Plugin '-Base';
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 const class_id => 'infobox';
 const class_title => 'Kwiki Infobox';
@@ -42,13 +42,36 @@ const class_title => 'Kwiki Infobox';
 sub register {
     my $registry = shift;
     $registry->add(wafl => infoblock => 'Kwiki::Infobox::WaflBlock');
+    $registry->add(wafl => infoblock_right => 'Kwiki::Infobox::WaflBlockRight');
     $registry->add(wafl => infobox => 'Kwiki::Infobox::WaflPhrase');
+    $registry->add(wafl => infobox_right => 'Kwiki::Infobox::WaflPhraseRight');
 }
 
 sub html {
     my $content = $self->pages->current->content;
     my $html;
-    my ($box) = $content =~ /{infobox:\s*(.+)\s*}/;
+    $self->hub->load_class('formatter');
+    while($content =~ /{infobox:\s*(.+)\s*}/g) {
+	my $boxpage = $self->pages->new_page($1)->content;
+	$html .= "<div class=\"infobox\">".
+	    $self->hub->formatter->text_to_html($boxpage)
+		. "</div>";
+    }
+
+    while($content =~ /\n.infoblock\n(.+\n).infoblock/sg) {
+	$html .= "<div class=\"infoblock\">" .
+	    $self->hub->formatter->text_to_html($1)
+		. "</div>";
+    }
+    return $html;
+}
+
+sub left { $self->html(@_) };
+
+sub right {
+    my $content = $self->pages->current->content;
+    my $html;
+    my ($box) = $content =~ /{infobox_right:\s*(.+)\s*}/;
     $self->hub->load_class('formatter');
     if($box) {
 	my $boxpage = $self->pages->new_page($box)->content;
@@ -57,7 +80,7 @@ sub html {
 		. "</div>";
     }
 
-    my ($block) = $content =~ /\n.infoblock\n(.+\n).infoblock/s;
+    my ($block) = $content =~ /\n.infoblock_right\n(.+\n).infoblock/s;
     if($block) {
 	$html .= "<div class=\"infoblock\">" .
 	    $self->hub->formatter->text_to_html($block)
@@ -68,15 +91,19 @@ sub html {
 
 package Kwiki::Infobox::WaflPhrase;
 use base 'Spoon::Formatter::WaflPhrase';
+sub to_html {'';}
 
+package Kwiki::Infobox::WaflPhraseRight;
+use base 'Spoon::Formatter::WaflPhrase';
 sub to_html {'';}
 
 package Kwiki::Infobox::WaflBlock;
 use base 'Spoon::Formatter::WaflBlock';
-
 sub to_html {'';}
 
-1;
+package Kwiki::Infobox::WaflBlockRight;
+use base 'Spoon::Formatter::WaflBlock';
+sub to_html {'';}
 
 =head1 COPYRIGHT
 
